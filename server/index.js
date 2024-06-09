820fcef1c884361ff0818b3cd2c8921df40c6247
Varun Gupta <varungupta1228@gmail.com>
2024-06-09 10:08:06 +0530

    add cors

--- server/index.js
+++ server/index.js
@@ -31,6 +31,18 @@ app.use(
   })
 );
 
+
+app.use(
+  cors({
+    origin: 'https://edtech-platform-varungupta.vercel.app', // use your actual domain name (or localhost), using * is not recommended
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'HEAD', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Origin', 'X-Requested-With', 'Accept', 'x-client-key', 'x-client-token', 'x-client-secret', 'Authorization'],
+    credentials: true,
+    maxAge: 14400,
+  })
+)
+
+
 app.use(
   fileUpload({
     useTempFiles: true,
